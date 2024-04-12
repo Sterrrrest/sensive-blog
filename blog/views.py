813c1a2b252be21217750151086f14b0558c2e90
Sterrrrest <sterrrrest@gmail.com>
2024-04-12 13:33:44 +0300

    Reduce queries count with prefetch_related for author

--- blog/views.py
+++ blog/views.py
@@ -35,10 +35,10 @@ def serialize_tag(tag):
 def index(request):
     # posts = Post.objects.annotate(num_likes_post=Count('likes'))
     # popular_posts = sorted(posts, key=get_likes_post)
-    most_popular_posts = Post.objects.annotate(num_likes=Count('likes')).order_by('-num_likes')[:5]
+    most_popular_posts = Post.objects.annotate(num_likes=Count('likes')).order_by('-num_likes')[:5].\
+        prefetch_related('author')
 
-
-    fresh_posts = Post.objects.order_by('published_at')
+    fresh_posts = Post.objects.order_by('published_at').prefetch_related('author')
     most_fresh_posts = list(fresh_posts)[-5:]
 
     # tags = Tag.objects.all()
