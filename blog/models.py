711b2251d2bc3c2888ec2ac1ed29b89f0ad1ffb5
Sterrrrest <sterrrrest@gmail.com>
2024-04-22 12:12:54 +0300

    Delete useless code

--- blog/models.py
+++ blog/models.py
@@ -52,7 +52,7 @@ class Post(models.Model):
         verbose_name='Теги')
 
     objects = PostQuerySet.as_manager()
-    
+
     def __str__(self):
         return self.title
 
--- blog/views.py
+++ blog/views.py
@@ -7,22 +7,6 @@ def get_related_posts_count(tag):
     return tag.posts.count()
 
 
-# def get_likes_post(post):
-#     return post.likes.count()
-
-
-def serialize_post(post):
-    return {
-        'title': post.title,
-        'teaser_text': post.text[:200],
-        'author': post.author.username,
-        'comments_amount': len(Comment.objects.filter(post=post)),
-        'image_url': post.image.url if post.image else None,
-        'published_at': post.published_at,
-        'slug': post.slug,
-        'tags': [serialize_tag_optimized(tag) for tag in post.tags.all()],
-        'first_tag_title': post.tags.all()[0].title,
-    }
 
 def serialize_post_optimized(post):
     return {
@@ -37,11 +21,6 @@ def serialize_post_optimized(post):
         'first_tag_title': post.tags.all()[0].title,
     }
 
-def serialize_tag(tag):
-    return {
-        'title': tag.title,
-        'posts_with_tag': Post.objects.filter(tags=tag).count(),
-    }
 
 def serialize_tag_optimized(tag):
     return {
