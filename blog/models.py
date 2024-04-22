466397142d0a6171cc06e94db7bc20eaa4886b96
Sterrrrest <sterrrrest@gmail.com>
2024-04-22 12:09:41 +0300

    Deleted useless code

--- blog/models.py
+++ blog/models.py
@@ -17,7 +17,7 @@ class PostQuerySet(models.QuerySet):
     def fetch_with_comments_count(self):
 
         most_popular_posts_ids = [post.id for post in self]
-        #
+        
         posts_with_comments = Post.objects.filter(id__in=most_popular_posts_ids).annotate(
             comments_count=Count('comments'))
         ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
